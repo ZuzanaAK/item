c255191d3c3fbee4c309718d1d209a4cca645f73
ZuzanaAK <kubatovazuzana@gmail.com>
2020-12-30 16:14:52 +0100

    google maps

--- routes/file-upload.routes.js
+++ routes/file-upload.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router  = express.Router();
 const Item = require('../models/Item.model');
 const mongoose = require('mongoose');
+const axios = require('axios');
 
 
 //Mongoose set up
@@ -19,6 +20,21 @@ router.post('/item-new', uploader.single("image"), (req, res, next) => {
  //You will get the image url in 'req.file.path'
  //store that in the DB 
   //console.log("body is: ", req.body)
+  const address = req.body.location;
+  console.log("the address is:", address)
+  const google_key = process.env.GOOGLE_MAPS_API_KEY
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${google_key}`
+  axios.get(url)
+  .then(response => {
+    console.log(response.data.results)
+   // const formattedAddress = ;
+    const coordinates = response.data.results[0].geometry.location;
+    console.log(coordinates);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
   Item.create({ item: req.body.item, category: req.body.category, image: req.file.path, description: req.body.description, user: req.session.passport.user, operation: req.body.operation })
 
       .then(() => {
